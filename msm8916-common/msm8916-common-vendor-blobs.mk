a5d21671b47d299d498b4759f9ca36606fc49bd9
Alberto97 <albertop2197@gmail.com>
2015-12-07 13:27:09 +0100

    msm8916-common: Move etc/data to vendor
    Change-Id: Idab5a36fa34276c1ef7bdbcc43805f1aaabfe5ba

--- msm8916-common/msm8916-common-vendor-blobs.mk
+++ msm8916-common/msm8916-common-vendor-blobs.mk
@@ -47,6 +47,9 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/msm8916-common/proprietary/etc/acdbdata/Headset_cal.acdb:system/etc/acdbdata/Headset_cal.acdb \
     vendor/motorola/msm8916-common/proprietary/etc/acdbdata/Speaker_cal.acdb:system/etc/acdbdata/Speaker_cal.acdb \
     vendor/motorola/msm8916-common/proprietary/etc/cacert_location.pem:system/etc/cacert_location.pem \
+    vendor/motorola/msm8916-common/proprietary/etc/data/dsi_config.xml:system/etc/data/dsi_config.xml \
+    vendor/motorola/msm8916-common/proprietary/etc/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    vendor/motorola/msm8916-common/proprietary/etc/data/qmi_config.xml:system/etc/data/qmi_config.xml \
     vendor/motorola/msm8916-common/proprietary/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
     vendor/motorola/msm8916-common/proprietary/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
     vendor/motorola/msm8916-common/proprietary/etc/firmware/cpp_firmware_v1_1_1.fw:system/etc/firmware/cpp_firmware_v1_1_1.fw \
