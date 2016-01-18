b524adfab343d3a72dbb2e5136602afef93adc38
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-01-17 23:44:40 -0500

    merlin: initial vendor blob set
    Not actually taken from merlin. These are just the osprey
    blobs + extra Adreno 4xx blobs taken from lux.
    
    Change-Id: I8c631bca48f81e0f170d0ae41b487077dc2e9d44

--- msm8916-common/Android.mk
+++ msm8916-common/Android.mk
@@ -14,7 +14,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter osprey surnia,$(TARGET_DEVICE)),)
+ifneq ($(filter lux merlin osprey surnia,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := MotGeoFenceSvc
